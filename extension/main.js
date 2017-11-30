c37141f1ae1ca401594a8133b0cece59480150ac
luzi82 <luzi82@gmail.com>
2017-11-30 15:15:19 +0800

    WireBuyer

--- extension/main.js
+++ extension/main.js
@@ -1,26 +1,62 @@
 function PaperclipTasMain(){
 
-    this.tickNow = 0;
     this.tick=function(){
         //console.log("GOBAUPRZ humanFlag="+humanFlag);
         //console.log("UQTNBIZP unsoldClips="+unsoldClips);
-        this.tickNow = Date.now();
+        this.tickBegin();
+        
+        this.autoClipClick();
+        this.autoBuyWire();
         this.autoPrice();
         this.autoQuantum();
     };
+
+    this.tickNow = 0;
+    this.stage = "unknown";
+    this.tickBegin=function(){
+        this.tickNow = Date.now();
+        if(humanFlag==1){
+            this.stage = "human";
+        }else if(spaceFlag==1){
+            this.stage = "space";
+        }else{
+            this.stage = "earth";
+        }
+    }
+    
+    this.autoClipClick=function(){
+        if(this.stage!="human")return;
+        if(clipmakerLevel>=10)return;
+        clipClick(1);
+    };
+    
+    this.autoBuyWire=function(){
+        if(this.shouldBuyWire()){
+            buyWire();
+        }
+    }
+    this.shouldBuyWire=function(){
+        if(this.stage!="human")return false;
+        if(wire<1)return true;
+        if(clipRate<=0)return false;
+        if(wireCost>Math.ceil(wireBasePrice-5))return false; // hardcode
+        if(wire/clipRate>30)return false; // hardcode
+        return true;
+    }
     
     this.autoPriceCooldown = 0;
     this.autoPrice=function(){
-        if(!humanFlag)return;
+        if(this.stage!="human")return;
         if(wire<1)return;
         if(this.autoPriceCooldown>this.tickNow)return;
+        if(clipRate<=0)return;
         var positive = (clipRate-avgSales)>0;
         var clipLevel = unsoldClips/clipRate;
         if(positive && (clipLevel>4) && (margin>0.015)){
-            this.lowerPrice();
+            lowerPrice();
             this.autoPriceCooldown = this.tickNow+1000;
         }else if((!positive) && (clipLevel<2)){
-            this.raisePrice();
+            raisePrice();
             this.autoPriceCooldown = this.tickNow+1000;
         }
     };
