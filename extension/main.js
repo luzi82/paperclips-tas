11d51e67eb39d8196d76222b04610e58f303b0ab
luzi82 <luzi82@gmail.com>
2017-11-30 14:26:20 +0800

    make class

--- extension/main.js
+++ extension/main.js
@@ -1,45 +1,54 @@
-var tickNow = 0;
-function tick(){
-    //console.log("GOBAUPRZ humanFlag="+humanFlag);
-    //console.log("UQTNBIZP unsoldClips="+unsoldClips);
-    tickNow = Date.now();
-    autoPrice();
-    autoQ();
+function PaperclipTasMain(){
+
+    this.tickNow = 0;
+    this.tick=function(){
+        //console.log("GOBAUPRZ humanFlag="+humanFlag);
+        //console.log("UQTNBIZP unsoldClips="+unsoldClips);
+        this.tickNow = Date.now();
+        this.autoPrice();
+        this.autoQ();
+    };
+    
+    this.autoPriceCooldown = 0;
+    this.autoPrice=function(){
+        if(!humanFlag)return;
+        if(wire<1)return;
+        if(this.autoPriceCooldown>this.tickNow)return;
+        var positive = (clipRate-avgSales)>0;
+        var clipLevel = unsoldClips/clipRate;
+        if(positive && (clipLevel>4) && (margin>0.015)){
+            this.lowerPrice();
+            this.autoPriceCooldown = this.tickNow+1000;
+        }else if((!positive) && (clipLevel<2)){
+            this.raisePrice();
+            this.autoPriceCooldown = this.tickNow+1000;
+        }
+    };
+    
+    this.autoQ=function(){
+        var calQ = this.calQComp();
+        if(calQ>0){
+            qComp();
+        }
+    };
+    
+    this.calQComp=function(){
+        if(qChips[0].active == 0) return 0;
+        var q = 0;
+        for (var i = 0; i<qChips.length; i++){
+            q = q+qChips[i].value;
+        }
+        var qq = Math.ceil(q*360);
+        return qq;
+    };
+    
+    this.start=function(){
+        var tthis = this;
+        setInterval(function(){tthis.tick();},200);
+        console.log("Paperclips TAS started");
+    };
+
 }
 
-var autoPriceCooldown = 0;
-function autoPrice(){
-    if(!humanFlag)return;
-    if(wire<1)return;
-    if(autoPriceCooldown>tickNow)return;
-    var positive = (clipRate-avgSales)>0;
-    var clipLevel = unsoldClips/clipRate;
-    if(positive && (clipLevel>4) && (margin>0.015)){
-        lowerPrice();
-        autoPriceCooldown = tickNow+1000;
-    }else if((!positive) && (clipLevel<2)){
-        raisePrice();
-        autoPriceCooldown = tickNow+1000;
-    }
-}
-
-function autoQ(){
-    var calQ = calQComp();
-    if(calQ>0){
-        qComp();
-    }
-}
-
-function calQComp(){
-    if(qChips[0].active == 0) return 0;
-    var q = 0;
-    for (var i = 0; i<qChips.length; i++){
-        q = q+qChips[i].value;
-    }
-    var qq = Math.ceil(q*360);
-    return qq;
-}
-
-setInterval(tick,200);
-
-console.log("Paperclips TAS started");
+var paperclipTasMain = new PaperclipTasMain();
+paperclipTasMain.start();
